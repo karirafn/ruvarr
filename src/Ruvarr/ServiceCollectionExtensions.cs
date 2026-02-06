ddccbed15994531f26c65d81c7ae246911408f33
Kári Rafn Karlsson <karirafn@gmail.com>
2026-02-06 17:03:46 +0000

    added AddRuvarr extension method

---
+++ src/Ruvarr/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+using Ruvarr.Internals.Clients;
+using Ruvarr.Internals.Options;
+
+namespace Ruvarr;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddRuvarr(this IServiceCollection services)
+    {
+        services.AddOptions<RuvarrOptions>()
+            .Configure<IConfiguration>((options, configuration)
+                => configuration.GetRequiredSection(RuvarrOptions.SectionName).Bind(options));
+
+        IOptions<RuvarrOptions> options = services.BuildServiceProvider()
+            .GetRequiredService<IOptions<RuvarrOptions>>();
+
+        services.AddTransient<IRuvClient, RuvClient>();
+        services.AddHttpClient<IRuvClient, RuvClient>(client => client.BaseAddress = options.Value.Ruv.ApiBaseAddress);
+
+        return services;
+    }
+}
