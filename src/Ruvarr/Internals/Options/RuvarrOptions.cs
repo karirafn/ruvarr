e48731d24e75b28b1e6ea4cda46b7629db23552e
Kári Rafn Karlsson <karirafn@gmail.com>
2026-02-06 17:02:46 +0000

    added RuvarrOptions

---
+++ src/Ruvarr/Internals/Options/RuvarrOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace Ruvarr.Internals.Options;
+
+internal sealed class RuvarrOptions
+{
+    public const string SectionName = "Ruvarr";
+
+    public required RuvOptions Ruv { get; init; }
+
+    public required TvdbOptions Tvdb { get; init; }
+
+    public required FfmpegOptions Ffmpeg { get; init; }
+}
