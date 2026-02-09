2d274dfe5f533824741cb84f3b622de812651790
Kári Rafn Karlsson <karirafn@gmail.com>
2026-02-09 15:23:56 +0000

    added DI

--- src/Ruvarr.Console/Program.cs
+++ src/Ruvarr.Console/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+using Ruvarr;
+
+IHostBuilder builder = Host.CreateDefaultBuilder();
+
+builder.ConfigureHostConfiguration(host => host.AddUserSecrets<Program>());
+
+builder.ConfigureServices((context, services) =>
+{
+    services.AddRuvarr(context.Configuration.GetConnectionString("Default")
+        ?? throw new ArgumentException("Connection string not found"));
+});
+
+IHost app = builder.Build();
+
+await app.RunAsync()
+    .ConfigureAwait(false);
