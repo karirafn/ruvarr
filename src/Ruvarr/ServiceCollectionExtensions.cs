561af0c31cf7516e1c842471272a7c06942e597a
Kári Rafn Karlsson <karirafn@gmail.com>
2026-02-09 15:23:32 +0000

    registered RuvSeriesSyncJob and TvdbLookupJob

--- src/Ruvarr/ServiceCollectionExtensions.cs
+++ src/Ruvarr/ServiceCollectionExtensions.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+using Quartz;
 
 using Ruvarr.FFmpeg;
+using Ruvarr.Jobs;
 using Ruvarr.Ruv;
 using Ruvarr.Tvdb;
 
@@ -8,12 +12,33 @@ namespace Ruvarr;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddRuvarr(this IServiceCollection services)
+    public static IServiceCollection AddRuvarr(this IServiceCollection services, string connectionString)
     {
         services.AddMemoryCache();
         services.AddFfmpeg();
         services.AddTvdb();
         services.AddRuv();
+
+        services.AddQuartz(options =>
+        {
+            JobKey seriesSync = new(nameof(RuvSeriesSyncJob));
+            options.AddJob<RuvSeriesSyncJob>(x => x.WithIdentity(seriesSync))
+                .AddTrigger(trigger => trigger
+                    .ForJob(seriesSync)
+                    .StartNow()
+                    .WithSimpleSchedule(x => x.WithIntervalInHours(6)
+                    .RepeatForever()));
+
+            JobKey tvdbLookup = new(nameof(TvdbLookupJob));
+            options.AddJob<TvdbLookupJob>(x => x.WithIdentity(tvdbLookup))
+                .AddTrigger(trigger => trigger
+                    .ForJob(tvdbLookup)
+                    .StartNow()
+                    .WithSimpleSchedule(x => x.WithIntervalInSeconds(5)
+                    .RepeatForever()));
+
+        });
+        services.AddQuartzHostedService(x => x.WaitForJobsToComplete = true);
 
         services.AddDbContext<RuvarrDbContext>(options =>
         {
