ac644eca2885d34df6ea28a66cb4db24b2bca2ec
Kári Rafn Karlsson <karirafn@gmail.com>
2026-02-19 17:16:11 +0000

    added IntegrationTestFactory

---
+++ tests/Ruvarr.IntegrationTests/IntegrationTestFactory.cs
@@ -0,0 +1,46 @@
+﻿
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ruvarr.IntegrationTests;
+
+internal sealed class IntegrationTestFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private readonly string _dbPath = Path.Combine(Path.GetTempPath(), $"{Guid.NewGuid()}.db");
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            ServiceDescriptor? descriptor = services.SingleOrDefault(
+                d => d.ServiceType == typeof(DbContextOptions<RuvarrDbContext>));
+
+            if (descriptor is not null)
+            {
+                services.Remove(descriptor);
+            }
+
+            services.AddDbContext<RuvarrDbContext>(options =>
+                options.UseSqlite($"Data Source={_dbPath}"));
+        });
+    }
+
+    public async ValueTask InitializeAsync()
+    {
+        using IServiceScope scope = Services.CreateScope();
+        RuvarrDbContext dbContext = scope.ServiceProvider.GetRequiredService<RuvarrDbContext>();
+        await dbContext.Database.MigrateAsync();
+    }
+
+    public new async Task DisposeAsync()
+    {
+        if (File.Exists(_dbPath))
+        {
+            File.Delete(_dbPath);
+        }
+
+        await base.DisposeAsync();
+    }
+}
