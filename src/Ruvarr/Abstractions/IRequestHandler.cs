a3bb8374d16d7f718636d9e3827581bd696e4248
Kári Rafn Karlsson <karirafn@gmail.com>
2026-03-07 11:03:40 +0000

    added IRequestHandler

---
+++ src/Ruvarr/Abstractions/IRequestHandler.cs
@@ -0,0 +1,11 @@
+﻿namespace Ruvarr.Abstractions;
+
+public interface IRequestHandler<TRequest>
+{
+    Task<RuvarrResult> Handle(TRequest request, CancellationToken cancellationToken);
+}
+
+public interface IRequestHandler<TRequest, TResponse>
+{
+    Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken);
+}
