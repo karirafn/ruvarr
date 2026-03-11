a4dab45aa7e5c83bb25d8206fe9ba5dd97c4e461
Kári Rafn Karlsson <karirafn@gmail.com>
2026-03-11 16:26:27 +0000

    refactor: added conversion to string for download queue status

--- src/Ruvarr/Migrations/20260311162325_DownloadQueueStatusAsString.Designer.cs
+++ src/Ruvarr/Migrations/20260311162325_DownloadQueueStatusAsString.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Ruvarr;
 
@@ -10,9 +11,11 @@ using Ruvarr;
 namespace Ruvarr.Migrations
 {
     [DbContext(typeof(RuvarrDbContext))]
-    partial class RuvarrDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260311162325_DownloadQueueStatusAsString")]
+    partial class DownloadQueueStatusAsString
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.4");
---
+++ src/Ruvarr/Migrations/20260311162325_DownloadQueueStatusAsString.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ruvarr.Migrations
+{
+    /// <inheritdoc />
+    public partial class DownloadQueueStatusAsString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "status",
+                table: "download_queue",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "status",
+                table: "download_queue",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+    }
+}
