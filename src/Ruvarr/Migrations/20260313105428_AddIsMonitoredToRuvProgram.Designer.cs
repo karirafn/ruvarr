06e0aef39640ea9359061cd636506fd02a973dfd
Kári Rafn Karlsson <karirafn@gmail.com>
2026-03-13 11:00:11 +0000

    feat: added IsMonitored to RuvProgram that is updated whenever a program is refreshed. (#37)

--- src/Ruvarr/Migrations/20260313105428_AddIsMonitoredToRuvProgram.Designer.cs
+++ src/Ruvarr/Migrations/20260313105428_AddIsMonitoredToRuvProgram.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Ruvarr;
 
@@ -10,9 +11,11 @@ using Ruvarr;
 namespace Ruvarr.Migrations
 {
     [DbContext(typeof(RuvarrDbContext))]
-    partial class RuvarrDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260313105428_AddIsMonitoredToRuvProgram")]
+    partial class AddIsMonitoredToRuvProgram
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.4");
